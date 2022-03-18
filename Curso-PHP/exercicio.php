96cf6bc3ed5a15044ea34a23b2a3f05399df3f06
Paulo Gadelha <pc.gadelha@hotmail.com>
2022-03-18 17:53:30 -0300

    inicio das paginas exercicio
    Inicio da pagina exercícios com inserção de botões

---
+++ Curso-PHP/exercicio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"/>
+    <link rel="stylesheet" href="../Curso-PHP/assets/css/estilo.css"/>
+    <link rel="stylesheet" href="assets/css/exercicio.css"/>
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem Formatação</a>
+        <a href="\index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+    </main>
+    <footer class="rodape">
+        C0D3R & ALUNOS &copy; <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
